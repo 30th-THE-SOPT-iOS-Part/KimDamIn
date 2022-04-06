9e9382c788ffda275890b436d4310b633222da7e
김담인 <>
2022-04-07 01:28:04 +0900

    read me 파일 수정 for Test

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 김담인 on 2022/04/07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
