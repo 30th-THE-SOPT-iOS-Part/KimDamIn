3d6d9425a24f1aadef86b87b0cdf962f90a9f197
Damin <ekadls757@gmail.com>
2022-04-15 20:49:37 +0900

    commit test

---
+++ iOS_Assignment2/iOS_assignment2/iOS_assignment2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOS_assignment2
+//
+//  Created by 김담인 on 2022/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let a = 0;
+        
+    }
+
+
+}
+
