a681d212d936323d69741c6f5586801619a16136
Damin <ekadls757@gmail.com>
2022-04-17 19:24:13 +0900

    2차 세미나 과제 커밋
    2차 세미나 과제 커밋

---
+++ iOS_Assignment2/iOS_assignment2/TabBars/MainTabViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainTabViewController.swift
+//  iOS_assignment2
+//
+//  Created by 김담인 on 2022/04/17.
+//
+
+import UIKit
+
+class MainTabViewController: UITabBarController, UITabBarControllerDelegate {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        
+        guard let reelsVC = UIStoryboard(name: "ReelsTab", bundle: nil).instantiateViewController(withIdentifier: "ReelsTabViewController") as? ReelsTabViewController else {return}
+        
+        tabBarController(self, didSelect: reelsVC)
+    }
+    
+    
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        
+       let tabBarIndex = tabBarController.selectedIndex
+        
+       if tabBarIndex == 2 {
+           tabBar.backgroundColor = .black
+           tabBar.barTintColor = .white
+           tabBar.tintColor = .white
+           tabBar.unselectedItemTintColor = .white
+       }else {
+           tabBar.backgroundColor = .white
+           tabBar.barTintColor = .black
+           tabBar.tintColor = .black
+           tabBar.unselectedItemTintColor = .black
+       }
+    }
+    
+}
+    
+
