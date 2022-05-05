3f6184f127b80afdcb31ab0f37600721ccb0bcd2
Damin <ekadls757@gmail.com>
2022-05-05 14:04:56 +0900

    commit test

---
+++ iOS_assignment3/iOS_assignment3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_assignment3
+//
+//  Created by 김담인 on 2022/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let commit = "commit"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ iOS_assignment3/iOS_assignment3UITests/iOS_assignment3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOS_assignment3UITestsLaunchTests.swift
+//  iOS_assignment3UITests
+//
+//  Created by 김담인 on 2022/05/05.
+//
+
+import XCTest
+
+class iOS_assignment3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
